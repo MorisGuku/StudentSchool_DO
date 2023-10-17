fb9c67e427d7a12a86cf2720503363a3b704a8df
MorisGuku <1nara5@mail.ru>
2023-10-17 17:28:08 +0300

    repear home work #2 i hate git-system

---
+++ StudentSchool_DO/hw_2_/Fibonachi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_2_
+{
+    public class Fibonachi
+    {
+        public static int GetFibonachi(int StepN)
+        {
+            int FirstNumberFibonachi = 0;
+            int SecondNumberFibonachi = 1;
+            for (int i = 0; i < StepN; i++)
+            {
+                int Temp = FirstNumberFibonachi;
+                FirstNumberFibonachi = SecondNumberFibonachi;
+                SecondNumberFibonachi = Temp + SecondNumberFibonachi;
+            }
+            return FirstNumberFibonachi;
+        }
+    }
+}
---
+++ StudentSchool_DO/hw_2_/ileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_2_
+{
+    internal class FileReader
+    {
+        public static string[] ReadLines(string filePath, int numberOfLines)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string[] result = new string[numberOfLines];
+                string line;
+                int counter = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    result[counter] = line;
+                    counter++;
+                    if (counter >= numberOfLines) break;
+                }
+                return result;
+            }
+
+        }
+    }
+}
